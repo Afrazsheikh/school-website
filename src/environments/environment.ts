837116f5da1d29e3d5ed8239c6f9709a21e93de5
birenyadav <afraz>
2023-02-06 22:22:18 +0530

    services

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiBaseUrl: 'http://localhost:3000/api/v1/',
+  imageBaseUrl: 'http://localhost:3000/api/v1/uploads/',
+  socketURL: 'http://localhost:3000',
 };
 
 /*
